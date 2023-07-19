6026e36f9d2c31d8d608a5c517d6ba7dc94da817
Siddarth Kumar <siddarthkay@gmail.com>
2023-07-19 20:32:01 +0530

    (iOS) upgrade react-native to 0.72.4

--- src/status_im/events.cljs
+++ src/status_im/events.cljs
@@ -44,7 +44,6 @@
     status-im.visibility-status-popover.core
     status-im.visibility-status-updates.core
     status-im.waku.core
-    status-im.wallet-connect.core
     status-im.wallet.accounts.core
     status-im.wallet.choose-recipient.core
     [status-im.wallet.core :as wallet]
--- src/status_im/wallet/choose_recipient/core.cljs
+++ src/status_im/wallet/choose_recipient/core.cljs
@@ -2,6 +2,7 @@
   (:require [re-frame.core :as re-frame]
             [status-im.bottom-sheet.events :as bottom-sheet]
             [status-im.contact.db :as contact.db]
+            [status-im.ethereum.core :as ethereum]
             [status-im.ethereum.eip681 :as eip681]
             [status-im.ethereum.ens :as ens]
             [utils.i18n :as i18n]
@@ -11,10 +12,8 @@
             [utils.url :as url]
             [utils.money :as money]
             [status-im.utils.universal-links.utils :as links]
-            [status-im.utils.wallet-connect :as wallet-connect]
             [status-im2.navigation.events :as navigation]
-            [utils.ethereum.chain :as chain]
-            [status-im.wallet.utils :as wallet.utils]))
+            [clojure.string :as string]))
 
 ;; FIXME(Ferossgp): Should be part of QR scanner not wallet
 (rf/defn toggle-flashlight
@@ -67,7 +66,7 @@
          :wallet/prepare-transaction
          (cond-> {:to      address
                   :to-name (or name (find-address-name db address))
-                  :from    (wallet.utils/get-default-account
+                  :from    (ethereum/get-default-account
                             (get db :profile/wallet-accounts))}
            gas       (assoc :gas (money/bignumber gas))
            gas-limit (assoc :gas (money/bignumber gas-limit))
@@ -107,7 +106,7 @@
                       {:success-callback
                        #(re-frame/dispatch
                          [:wallet.send/update-gas-price-success :wallet/prepare-transaction %])
-                       :network-id (get-in (chain/current-network db)
+                       :network-id (get-in (ethereum/current-network db)
                                            [:config :NetworkId])}})
                    (when (and chain-id (not= current-chain-id chain-id))
                      {:ui/show-error (i18n/label :t/wallet-invalid-chain-id
@@ -130,7 +129,7 @@
       ;; if there are no ens-names, we dispatch request-uri-parsed immediately
       (request-uri-parsed cofx message uri)
       {::resolve-addresses
-       {:chain-id (chain/chain-id db)
+       {:chain-id (ethereum/chain-id db)
         :ens-names ens-names
         :callback
         (fn [addresses]
@@ -145,7 +144,7 @@
       (if (links/universal-link? uri)
         {:dispatch [:universal-links/handle-url uri]}
         {:browser/show-browser-selection uri})
-      (if (wallet-connect/url? uri)
+      (if (string/starts-with? uri "wc:")
         {:ui/show-error "Wallet Connect not implemented"}
         ;; Re-enable with https://github.com/status-im/status-mobile/issues/13429
         ;; {:dispatch [::qr-scaner/handle-wallet-connect-uri {:data uri}]}
