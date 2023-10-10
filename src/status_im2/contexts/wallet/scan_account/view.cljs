01652944b1e78bf5152758bff8e8d4d38d073e1c
Brian Sztamfater <brian@status.im>
2023-10-10 11:13:10 -0300

    support different qr formats and extract the address

--- src/status_im2/contexts/wallet/scan_account/view.cljs
+++ src/status_im2/contexts/wallet/scan_account/view.cljs
@@ -2,7 +2,20 @@
   (:require [utils.debounce :as debounce]
             [utils.i18n :as i18n]
             [status-im2.common.scan-qr-code.view :as scan-qr-code]
-            [status-im2.constants :as constants]))
+            [status-im2.constants :as constants]
+            [clojure.string :as string]))
+
+(defn- is-address
+  [s]
+  (boolean (re-matches constants/regx-address s)))
+
+(defn- has-address?
+  [scanned-text]
+  (some #(is-address %) (string/split scanned-text #":")))
+
+(defn- extract-address
+  [scanned-text]
+  (first (filter #(is-address %) (string/split scanned-text #":"))))
 
 (defn view
   []
@@ -10,5 +23,7 @@
    {:title           (i18n/label :t/scan-qr)
     :subtitle        (i18n/label :t/scan-an-account-qr-code)
     :error-message   (i18n/label :t/oops-this-qr-does-not-contain-an-address)
-    :validate-fn     #(boolean (re-matches constants/regx-address %))
-    :on-success-scan #(debounce/debounce-and-dispatch [:wallet-2/scan-address-success %] 300)}])
+    :validate-fn     #(has-address? %)
+    :on-success-scan #(debounce/debounce-and-dispatch [:wallet-2/scan-address-success
+                                                       (extract-address %)]
+                                                      300)}])
